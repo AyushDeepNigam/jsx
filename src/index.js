7730640a65299741e0c06904a5e59b4dbb4cc6f1
Ayush Deep Nigam <aayushdeepnigam@gmail.com>
2021-01-08 20:04:11 +0530

    Approval Card

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
-// Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-// Create a React component
 const App = () => {
-    const buttonText = {text: 'Click me'};
-    return (
-        <div>
-        <label className = "label" htmlFor = "name">Enter: </label>
-        <input id = "name" type="text"/>
-        <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
-       </div>
-    );
+  return (
+    <div className="ui containter comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Nice!"
+          imga={
+            "https://i.pinimg.com/originals/60/4c/f8/604cf86d3a59da3b75a09d17e224f5f1.jpg"
+          }
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 4:59PM"
+          comment="Nice post!"
+          imga={
+            "https://pa1.narvii.com/6604/e1aafd6a255b485e4cbb4ddb3213bb46b95b6486_00.gif"
+          }
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 5:12PM"
+          comment="Nice post brah!"
+          imga={"https://data.whicdn.com/images/320966807/original.jpg"}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
-
-// Take the React component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
